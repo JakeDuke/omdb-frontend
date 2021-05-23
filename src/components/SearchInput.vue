c07b0787a1361737761e95308e911861fa8a17ed
Jake Dukin <jake.dukin@bohemian.studio>
2021-05-23 22:40:33 +0200

    initial

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="input">
+    <b-form-input v-model="text" placeholder="Enter movie name"></b-form-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchInput",
+  data() {
+    return {
+      text: "",
+      url: "http://www.omdbapi.com/?i=tt3896198&apikey=f68f2cab",
+    };
+  },
+  mounted() {
+    this.axios.get(this.url);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+.input {
+  max-width: 300px;
+}
+</style>
