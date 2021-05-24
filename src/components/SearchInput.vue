bfb24d7558c68c6a49463570c4980f3de6d3eba7
Jake Dukin <jake.dukin@bohemian.studio>
2021-05-24 15:39:48 +0200

    search results added

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,25 +1,43 @@
 <template>
   <div class="main">
-    <div class="input-wrap">
-      <label for="search">Movie name:</label>
-      <b-form-input
-        v-model="movieName"
-        placeholder="Enter movie name"
-        debounce="1000"
-        id="search"
-      ></b-form-input>
+    <div class="inputs">
+      <div class="input-wrap">
+        <label for="search">Movie name:</label>
+        <b-form-input
+          v-model="movieName"
+          placeholder="Enter movie name"
+          debounce="1000"
+          id="search"
+        ></b-form-input>
+      </div>
+      <div class="input-wrap">
+        <label for="year">Year:</label>
+        <b-form-input
+          v-model="year"
+          placeholder="Year"
+          id="year"
+        ></b-form-input>
+      </div>
+      <div class="input-wrap">
+        <label for="type">Type:</label>
+        <b-form-select v-model="type" :options="typeOptions" id="type">
+          <template #first>
+            <b-form-select-option value=""
+              >-- Please select type --</b-form-select-option
+            >
+          </template>
+        </b-form-select>
+      </div>
     </div>
-    <div class="input-wrap">
-      <label for="year">Year:</label>
-      <b-form-input v-model="year" placeholder="year" id="year"></b-form-input>
-    </div>
-    <div class="input-wrap">
-      <label for="type">Type:</label>
-      <b-form-select
-        v-model="type"
-        :options="typeOptions"
-        id="type"
-      ></b-form-select>
+    <div class="results">
+      <b-table stacked :items="results">
+        <template #cell(poster)="data">
+          <img :src="data.item.Poster" alt="img" />
+        </template>
+        <template #cell(ratings)="data">
+          <b-table stacked :items="data.item.Ratings"> </b-table>
+        </template>
+      </b-table>
     </div>
   </div>
 </template>
@@ -34,6 +52,8 @@ export default {
       type: "",
       typeOptions: ["movie", "series", "episode"],
       baseUrl: "http://www.omdbapi.com/?i=tt3896198&apikey=f68f2cab",
+      loading: null,
+      results: [],
     };
   },
   computed: {
@@ -41,15 +61,20 @@ export default {
       return (
         this.baseUrl +
         `&t=${this.movieName}` +
-        `&y=${this.year}` +
-        `&type=${this.type}`
+        `${this.year && `&y=${this.year}`}` +
+        `${this.type && `&type=${this.type}`}`
       );
     },
   },
   methods: {
     async getMovie() {
-      const data = await this.axios.get(this.url);
-      console.log(data, "data");
+      this.results = [];
+      try {
+        const { data } = await this.axios.get(this.url);
+        this.results.push(data);
+      } catch (e) {
+        console.log(e);
+      }
     },
   },
   watch: {
@@ -62,6 +87,10 @@ export default {
 
 <style lang="scss" scoped>
 .main {
+  display: block;
+  width: 100%;
+}
+.inputs {
   display: flex;
 }
 
@@ -75,5 +104,10 @@ export default {
     margin-right: 0.5rem;
     margin-bottom: 0;
   }
+}
+
+.results {
+  width: 100%;
+  overflow: auto;
 }
 </style>
